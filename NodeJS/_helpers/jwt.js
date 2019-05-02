ff6961436877bc2f0d85373497992f6879ba634c
janvipalan <janvipalan@tamu.edu>
2019-05-02 17:50:27 -0500

    changes

--- NodeJS/_helpers/jwt.js
+++ NodeJS/_helpers/jwt.js
@@ -1,6 +1,6 @@
 const expressJwt = require('express-jwt');
-const config = require('config.json');
-const userService = require('../users/user.service');
+const config = require('../config.json');
+const userService = require('../controllers/user.service');
 
 module.exports = jwt;
 
